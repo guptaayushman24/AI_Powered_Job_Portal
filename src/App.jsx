095365c2baf23ced92f303b435b9939085bc9e91
Ayushman Gupta <guptaayushman24@gmail.com>
2025-01-12 10:38:02 +0530

    Landing_Page is created

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Landing_Page from '../src/components/Landing_Page'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <Landing_Page></Landing_Page>
+    </>
+  )
+}
+
+export default App
