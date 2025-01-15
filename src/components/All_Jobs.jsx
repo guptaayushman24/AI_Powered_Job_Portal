b7c9b532fa49b747b2ebfbe00817ba9e39c6fc8d
Ayushman Gupta <guptaayushman24@gmail.com>
2025-01-15 12:50:02 +0530

    Pagination implemented and Job is Displayed

--- src/components/All_Jobs.jsx
+++ src/components/All_Jobs.jsx
@@ -3,11 +3,18 @@ import axios, { all } from 'axios';
 import './All_Jobs.css'
 function AllJobs(){
     const [alldata,setalldata] = useState([]);
-    const [totalPages,settotalPages] = useState(0);
+    const [page,setPage] = useState(1);
+    // const []
     async function showalljobs(){
         const data = await axios.get('http://localhost:5000/alljobs');
         setalldata(data.data.Job_Data);
         console.log(data);
+    }
+    function selectPageHandler(selectedPage){
+        if (selectedPage>=1 && selectedPage<=alldata.length/5 && selectedPage!=page){
+            setPage(selectedPage);
+        }
+        
     }
     useEffect(()=>{
         showalljobs();
@@ -29,15 +36,15 @@ function AllJobs(){
                     
 
                     {
-                        alldata.map((jobs,index)=>{
+                        alldata.slice(page*5-5,page*5).map((jobs,index)=>{
                             return(
                                 <div className="alljobs-parent">
                                 <div className="alljobs-cards">
                         <div className="alljobs-details">
-                            <div>Company Name:-</div>
-                            <div>Salary:-</div>
-                            <div>Location:-</div>
-                            <div>Type Of Job:-</div>
+                            <div>Company Name:- {jobs.CompanyName}</div>
+                            <div>Salary:- {jobs.Salary}{" "}{"Lakh"}</div>
+                            <div>Location:- {jobs.Location}</div>
+                            <div>Type Of Job:- {jobs.TypeofJob}</div>
                         </div>
                         <div className="alljobs-buttons">
                             <div className="alljobs-scorebutton">
@@ -55,6 +62,20 @@ function AllJobs(){
                     }
                     </div>
                 </div>
+            </div>
+            <div className="pagination">
+            <span
+            className={page>1?"":"pagination_disable"}
+             onClick={()=>selectPageHandler(page-1)}>←</span>
+            {
+               [...Array(alldata.length/5)].map((_,i)=>{
+                 return <span className={page===i+1?"paginationselected":""}
+                  onClick={()=>selectPageHandler(i+1)} key={i}>{i+1}</span>
+               })
+            }
+            <span
+             className={page<alldata.length/5?"":"pagination_disable"}
+             onClick={()=>selectPageHandler(page+1)}>→</span>
             </div>
          </div>
         </>
